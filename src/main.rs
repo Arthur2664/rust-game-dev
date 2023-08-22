854b6d858c458b6e408f4b45d8ab3d17da799ff4
Arthur Yakymchuk <arthur.yakymchuk@gmail.com>
2023-08-22 22:25:33 +0300

    format usages

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
+mod fibonachi;
+mod guess_game;
+mod temp_conv;
 fn main() {
     let number = fibonachi::calc(19);
 
     println!("{number}");
 }
-
-mod guess_game;
-mod temp_conv;
-mod fibonachi;
